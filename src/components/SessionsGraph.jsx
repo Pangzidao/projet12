6944da7231582ea04d7b1247450a2059e56df918
Pangzidao <nicolasyvonlejeune@gmail.com>
2023-02-09 09:13:45 +0100

    commit

--- src/components/SessionsGraph.jsx
+++ src/components/SessionsGraph.jsx
@@ -14,12 +14,64 @@ import PropTypes from 'prop-types';
 function SessionsGraph(props){
 
     const sessionsData = props.data
+
+    
+    /**
+ * Format Tooltip
+ * @param {array} payload - source data
+ * @param {boolean} active - is Tootip active
+ * @returns the value when a dot on the line is pointed
+ */
+    function CustomToolTypeSessionDuration({payload, active}){
+      if(active){
+        console.log(payload[0].value)
+          return (
+              <div className='sessionDurationChartTooltip'>
+                  <div>{`${payload[0].value}`}min</div>
+              </div>
+          )
+      }
+      return null
+  }
+
+  /**
+ * Animate background format when moving the cursor on the chart line
+ * @param {event} e - move of the mouse
+ * @returns darker background from the pointed dot
+ */
+  function customMouseMove(e){
+    let sessionWrap = document.querySelector('.sessionDurationWrap');
+    if (e.isTooltipActive) {
+      let windowWidth = sessionWrap.offsetWidth;
+      let mouseXpercent = Math.floor(
+        (e.activeCoordinate.x / windowWidth) * 100
+      );
+      
+      sessionWrap.style.background = `linear-gradient(90deg, rgba(255,0,0, 1) ${mouseXpercent}%, rgba(0,0,0,0.1) ${mouseXpercent}%, rgba(0,0,0,0.1) 100%)`;
+    }
+    else{
+        sessionWrap.style.background ='red'
+    }
+}
+
+/**
+* Animate background format when moving the cursor out of a line dot
+* @param {event} e - move of the mouse
+* @returns initial background
+*/
+function customOnMouseOut(){
+    let sessionWrap = document.querySelector('.sessionDurationWrap');
+    sessionWrap.style.background ='red'
+}
     
     return(
-      <div className={styles.container}>
+      <div className="sessionDurationWrap">
         <h2 className={styles.title}>Durée moyenne des sessions</h2>
         <ResponsiveContainer width='100%' height={150}>
-            <LineChart width={500} height={300} data={sessionsData}>
+            <LineChart width={500} height={300} data={sessionsData}
+             onMouseMove={(e) => customMouseMove(e)}
+             onMouseOut={() => customOnMouseOut()}
+            >
               <XAxis tick={{fill:'white', fillOpacity:0.5}} padding={{ left: 20, right: 20 }} axisLine={false} tickLine={false} dataKey="day" textDecoration={{color:'white'}}/>
               <YAxis hide={true} padding={{ bottom:30}}/>
               <Tooltip 
@@ -35,6 +87,7 @@ function SessionsGraph(props){
                 return null;
                 }}
               />
+               <Tooltip content={<CustomToolTypeSessionDuration />} cursor={false} />
               <Line type="natural" dataKey="minutes" stroke="#FFFFFF" dot={false}/>
             </LineChart>
           </ResponsiveContainer>
